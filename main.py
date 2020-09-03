e4e96c5620dc6575e88ef32ee4b979fed928f0d6
jkv5212 <jkv5212@psu.edu>
2020-09-03 01:10:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#Author: Jason Vanderhoff jkv5212@psu.edu
+
+acc = 0;
+totCred = 0;
+def letter_grade(grade):
+  if(grade == "A"):
+    return 4
+
+  elif(grade == "A-"):
+    return 3.67
+
+  elif(grade == "B+"):
+    return 3.33
+
+  elif(grade == "B"):
+    return 3
+
+  elif(grade == "B-"):
+    return 2.67
+
+  elif(grade == "C+"):
+    return 2.33
+
+  elif(grade == "C"):
+    return 2
+
+  elif(grade == "D"):
+    return 1
+
+  else:
+    return 0
+
+
+for x in range(1, 4):
+  letter = input(f"Enter your course {x} letter grade: ")
+  cred = input(f"Enter your course {x} credit: ")
+  totCred = totCred + int(cred)
+  lGrade = letter_grade(letter)
+  acc = acc + lGrade * int(cred)
+  print(f"Grade point for course {x}: {lGrade}")
+
+print("Your GPA is " + str(acc/totCred))
+
